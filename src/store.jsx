b88b9af58f0b54b7e3998827889951eaaa508475
Matrix53 <1079207272@qq.com>
2021-08-01 12:47:18 +0800

    chore: update global state

---
+++ src/store.jsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialState = {
+  isLogin: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { isLogin: true };
+    case "logout":
+      return { isLogin: false };
+    default:
+      return new Error();
+  }
+}
+
+const StateContext = createContext();
+const DispatchContext = createContext();
+
+function useStateStore() {
+  return useContext(StateContext);
+}
+
+function useDispatchStore() {
+  return useContext(DispatchContext);
+}
+
+function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  );
+}
+
+export { useStateStore, useDispatchStore, StoreProvider };
