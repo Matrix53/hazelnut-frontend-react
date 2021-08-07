454cc672165074cceab75708feb9e894de4b9661
Matrix53 <1079207272@qq.com>
2021-08-07 11:59:45 +0800

    build: update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppBar>
+        <Tabs
+        >
+          <Tab label="Item One"/>
+          <Tab label="Item Two" />
+          <Tab label="Item Three"  />
+        </Tabs>
+      </AppBar>
+    </div>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "App";
 import StoreProvider from "store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StoreProvider>
   </React.StrictMode>,
   document.getElementById("root")
