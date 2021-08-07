a14d53832ed1f878e039c602d0651d03fcf2b2fe
Matrix53 <1079207272@qq.com>
2021-08-07 12:05:24 +0800

    chore: update mock server

--- src/http.js
+++ src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ab9e0ca6-2d18-4e74-a450-c59bc2fdaed1.mock.pstmn.io/",
+  baseURL: "https://94cbb646-c3fb-4ac3-8bc3-b1c4b12c8c0b.mock.pstmn.io/",
   timeout: 3000,
 });
 
