8ea0df1ad536be0123f208c75f80cfa47482f776
Matrix53 <1079207272@qq.com>
2021-08-13 01:09:23 +0800

    chore: practice some syntax

--- src/components/global/AppHeader.jsx
+++ src/components/global/AppHeader.jsx
@@ -1,18 +1,27 @@
 import { Button, IconButton, AppBar, Toolbar } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
-import logo from "assets/logo.png";
 import { useStateStore } from "store";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  logo: {
-    width: "5vmin",
-  },
+const useStyles = makeStyles((theme) => ({
   root: {
     height: "10vh",
+    lineHeight: "10vh",
+    background: theme.palette.primary.main,
   },
-});
+  title: {
+    height: "10vh",
+    textDecoration: "none",
+    color: "white",
+    flexGrow: 1,
+  },
+  link: {
+    height: "10vh",
+    color: "white",
+    marginRight: theme.spacing(2),
+  },
+}));
 
 function AppHeader() {
   const classes = useStyles();
@@ -21,11 +30,8 @@ function AppHeader() {
   return (
     <AppBar position="sticky" className={classes.root}>
       <Toolbar>
-        <Link to="/good">
-          <img src={logo} alt="Logo" className={classes.logo} />
-        </Link>
-        <Link to="/good">
-          <div>Hazelnut交易平台</div>
+        <Link to="/good" className={classes.title}>
+          Hazelnut交易平台
         </Link>
         {isLogin ? (
           <IconButton>
@@ -33,10 +39,10 @@ function AppHeader() {
           </IconButton>
         ) : (
           <>
-            <Button component={Link} to="/signin">
+            <Button component={Link} to="/signin" className={classes.link}>
               登录
             </Button>
-            <Button component={Link} to="/signup">
+            <Button component={Link} to="/signup" className={classes.link}>
               注册
             </Button>
           </>
