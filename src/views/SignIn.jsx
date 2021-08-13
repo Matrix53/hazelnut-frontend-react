9d8be680afd03f68035c52e83c51932b18e4022d
Matrix53 <1079207272@qq.com>
2021-08-13 10:38:15 +0800

    build: optimize development startup times

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,5 +1,6 @@
 import SignInForm from "components/auth/SignInForm";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
